c44133e5064dacb7724db5ae2a2d33ac6cdd012a
Murray Coueslant <murray.coueslant+github@gmail.com>
2017-09-18 13:08:25 -0400

    successfully created the random map generator, realising i may need a 3d array to store the visited boolean value too. implementing that now.

--- location_definition_test.py
+++ location_definition_test.py
@@ -2,26 +2,47 @@
 # Murray Coueslant, 2017/09/18
 
 # module imports
-from random import shuffle
+from random import shuffle, randint
 
 # variable and array definitions
 rows = 3
 cols = 3
-gameMap = [[0 for i in range(cols)] for j in range(rows)]
+gameMap = [[None for i in range(cols)] for j in range(rows)]
 mapLocations = ['a', 'b', 'c', 'd', 'e', 'f']
 
 # map generator
 
 
 def generateMap(locationList):
-    unusedLocations = locationList
-    numberOfLocations = len(unusedLocations)
+    locations = locationList
+    numberOfLocations = len(locations)
     orderList = list(range(numberOfLocations))
     shuffle(orderList)
-    
+    print(orderList)
+    for i in orderList:
+        assignLocation(locations[i], gameMap)
+    fillEmpty(gameMap)
+    print(gameMap)
+
 
 def assignLocation(location, map):
-    map[0][0] = location
+    rand_row = randint(0, 2)
+    rand_col = randint(0, 2)
+    placed = False
+    while not placed:
+        if map[rand_row][rand_col] == None:
+            map[rand_row][rand_col] = location
+            placed = True
+        else:
+            rand_row = randint(0, 2)
+            rand_col = randint(0, 2)
+
+
+def fillEmpty(map):
+    for j in range(cols):
+        for i in range(rows):
+            if map[i][j] == None:
+                map[i][j] = 'There is nothing here.'
 
 
 generateMap(mapLocations)
