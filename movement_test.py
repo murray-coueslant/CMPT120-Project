80d20af457ba464eea5b556726dc85c552943073
Murray Coueslant <murray.coueslant+github@gmail.com>
2017-09-18 21:35:24 -0400

    okay, this one is getting unwieldy now. i need to figure out exactly what i need rather than just adding features in. the game has its essential functions though, i am currently just working on the ending of the game.

--- movement_test.py
+++ movement_test.py
@@ -20,31 +20,52 @@ class Player:
             if self.rowLocation < 0:
                 collisionMessage(self)
                 self.rowLocation += 1
+            else:
+                visitLocation(self.rowLocation, self.colLocation, self)
         elif direction.lower() == 'south':
             self.rowLocation += 1
             if self.rowLocation > 2:
                 collisionMessage(self)
                 self.rowLocation -= 1
+            else:
+                visitLocation(self.rowLocation, self.colLocation, self)
         elif direction.lower() == 'east':
             self.colLocation += 1
             if self.colLocation > 2:
                 collisionMessage(self)
                 self.colLocation -= 1
+            else:
+                visitLocation(self.rowLocation, self.colLocation, self)
         elif direction.lower() == 'west':
             self.colLocation -= 1
             if self.colLocation < 0:
                 collisionMessage(self)
                 self.colLocation += 1
             else:
-                visitLocation()
+                visitLocation(self.rowLocation, self.colLocation, self)
         else:
             displayError(1, self)
+
+    def getName(self):
+        return self.name
 
     def getLocation(self, map):
         print(self.name, 'is currently at:', map.getLocation(self.colLocation, self.rowLocation))
 
+    def getXPos(self):
+        return self.colLocation
+
+    def getYPos(self):
+        return self.rowLocation
+
+    def getScore(self):
+        return self.score
+
     def increaseScore(self):
         self.score += 5
+
+    def displayScore(self):
+        print(self.getName(), 'your score is:', self.getScore())
 
 # map class definition
 
@@ -77,8 +98,17 @@ class map():
                 if self.map[i][j] == None:
                     self.map[i][j] = ['There is nothing here.', False]
 
-    def getLocation(self, xPos, yPos):
-        return self.map[yPos][xPos][0]
+    def getLocation(self, rowPos, colPos):
+        return self.map[rowPos][colPos][0]
+
+    def getVisited(self, rowPos, colPos):
+        return self.map[rowPos][colPos][1]
+
+    def setVisited(self, rowPos, colPos):
+        self.map[rowPos][colPos][1] = True
+
+    def getMap(self):
+        return self.map
 
 # variable and array definitions
 rows = 3
@@ -96,7 +126,7 @@ quitCommands = ['q', 'quit', 'exit', 'end', 'leave']
 # general game modules
 
 def collisionMessage(player):
-    print('Collision, you cannot move this way. Choose another direction', player.name)
+    print('Collision, you cannot move this way. Choose another direction', player.name + '.')
     return
 
 
@@ -136,15 +166,36 @@ def getCommand(player, command):
         getCommand(character, input("Enter new command: "))
 
 
-def visitLocation(locationRow, locationCol):
-    if 1:
-        return
+def visitLocation(locationRow, locationCol, player):
+    if gameMap.getVisited(locationRow, locationCol) is False:
+        player.increaseScore()
+        gameMap.setVisited(locationRow, locationCol)
+    elif gameMap.getVisited(locationRow, locationCol) is True:
+        print('You have already discovered this location!')
+def checkVisited(map):
+    visitedCount = 0
+    for j in range(cols):
+        for i in range(rows):
+            if map[i][j] == True:
+                visitedCount += 1
+    return visitedCount
+
+def endGame():
+    print('\n' + 'Congratulations, you have made it home safely. You rest for a few days and then return to normal '
+          'life.' + '\n')
+    displayCopyright()
+    print('I hope you enjoyed playing this game. See you soon!' + '\n')
 
 gameMap = map(3, 3, mapLocations)
 character = Player('Murray', 0, 1, 1)
 character.getLocation(gameMap)
-
+visitLocation(1, 1, character)
+character.displayScore()
 while 1:
     getCommand(character, input('What would you like to do?: '))
     character.getLocation(gameMap)
-
+    character.displayScore()
+    count = checkVisited(gameMap.getMap())
+    print(count)
+    if count == 9:
+        endGame()
