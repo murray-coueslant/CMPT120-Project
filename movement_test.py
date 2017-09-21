180fe99b4fe8a672fbc68be3c9f4dcdf10e82d72
Murray Coueslant <murray.coueslant+github@gmail.com>
2017-09-21 12:56:45 -0400

    bug on game startup where map is not assigned correctly due to out of range parameters, not sure why this is happening. seeing professor johnson about it because i have spent a lot of time thinking about it and trying things

--- movement_test.py
+++ movement_test.py
@@ -18,33 +18,42 @@ class Player:
         if direction.lower() == 'north':
             self.rowLocation -= 1
             if self.rowLocation < 0:
-                collisionMessage(self)
+                game.collisionMessage(self)
                 self.rowLocation += 1
             else:
-                visitLocation(self.rowLocation, self.colLocation, self)
+                self.visitLocation()
         elif direction.lower() == 'south':
             self.rowLocation += 1
             if self.rowLocation > (map.rowSize -1):
-                collisionMessage(self)
+                game.collisionMessage(self)
                 self.rowLocation -= 1
             else:
-                visitLocation(self.rowLocation, self.colLocation, self)
+                self.visitLocation()
         elif direction.lower() == 'east':
             self.colLocation += 1
             if self.colLocation > (map.colSize - 1):
-                collisionMessage(self)
+                game.collisionMessage(self)
                 self.colLocation -= 1
             else:
-                visitLocation(self.rowLocation, self.colLocation, self)
+                self.visitLocation()
         elif direction.lower() == 'west':
             self.colLocation -= 1
             if self.colLocation < 0:
-                collisionMessage(self)
+                game.collisionMessage(self)
                 self.colLocation += 1
             else:
-                visitLocation(self.rowLocation, self.colLocation, self)
+                self.visitLocation()
         else:
-            displayError(1, self)
+            game.displayError(1, self)
+
+    def visitLocation(self):
+        if map.getVisited(self.rowLocation, self.colLocation) == 'Flag':
+            map.setVisited(self.rowLocation, self.colLocation)
+        elif map.getVisited(self.rowLocation, self.colLocation) is False:
+            self.increaseScore()
+            map.setVisited(self.rowLocation, self.colLocation)
+        elif map.getVisited(self.rowLocation, self.colLocation) is True:
+            print('You have already discovered this location!')
 
     def getName(self):
         return self.name
@@ -69,6 +78,7 @@ class Player:
 
 # map class definition
 
+
 class map():
     def __init__(self, rowSize, colSize, locations):
         self.rowSize = rowSize
@@ -79,24 +89,34 @@ class map():
         orderList = list(range(numberOfLocations))
         shuffle(orderList)
         for i in orderList:
-            randRow = randint(0, 2)
-            randCol = randint(0, 2)
+            randRow = randint(0, (self.rowSize-1))
+            randCol = randint(0, (self.colSize-1))
+            print(randRow, randCol)
             placed = False
             while not placed:
                 if self.map[randRow][randCol] is None:
                     self.map[randRow][randCol] = [self.locations[i], False]
                     placed = True
                 else:
-                    randRow = randint(0, 2)
-                    randCol = randint(0, 2)
+                    randRow = randint(0, rowSize)
+                    randCol = randint(0, colSize)
         self.fillEmpty()
-
 
     def fillEmpty(self):
         for j in range(self.colSize):
             for i in range(self.rowSize):
-                if self.map[i][j] == None:
+                if self.map[i][j] is None:
                     self.map[i][j] = ['There is nothing here.', 'Flag']
+
+    def checkVisited(self):
+        visitedCount = 0
+        map = self.getMap()
+        cols, rows = self.getSizes()
+        for j in range(cols):
+            for i in range(rows):
+                if map[i][j][1] is True:
+                    visitedCount += 1
+        return visitedCount
 
     def getSizes(self):
         return self.colSize, self.rowSize
@@ -114,9 +134,55 @@ class map():
         return self.map
 
 
+# game class
+class game():
+    def collisionMessage(self, player):
+        print('Collision, you cannot move this way. Choose another direction', player.name + '.')
+        return
+
+    def displayError(self, messageNo, player):
+        if messageNo == 1:
+            message = ('Incorrect direction command entered, please enter another,', player.name)
+        else:
+            message = 'Unknown error.'
+        print(message)
+
+    def displayHelp(self):
+        print('Help:', '\n', 'Enter a command below, the possible commands are:', '\n', 'north, south, east, west'
+              '\n', 'go, move or travel + a direction', '\n', 'quit, exit, leave, end', '\n'
+              'or this help command, but you figured that one out, go you!')
+
+    def getCommand(self, player, command, map):
+        if command.lower() in northCommands:
+            player.movePlayer('north', map)
+        elif command.lower() in eastCommands:
+            player.movePlayer('east', map)
+        elif command.lower() in southCommands:
+            player.movePlayer('south', map)
+        elif command.lower() in westCommands:
+            player.movePlayer('west', map)
+        elif command.lower() in helpCommands:
+            self.displayHelp()
+        elif command.lower() in quitCommands:
+            input('Thanks for playing, press enter to end the game.')
+            quit()
+        elif command.lower() == '' or None:
+            print('Unrecognised command, enter another.')
+            self.getCommand(character, input('Enter new command: '))
+        else:
+            print('Unrecognised command, enter another.')
+            self.getCommand(character, input('Enter new command: '))
+
+    def endGame(self):
+        print('\n' + 'Congratulations, you have explored the whole island!' + '\n')
+        print(copyrightMessage)
+        print('I hope you enjoyed playing this game. See you soon!' + '\n')
+        quit()
+
+
 # variable and array definitions
-copyright = 'This game is property of Murray Coueslant. Any enquiries can be sent to murray.coueslant1@marist.edu. \
- Fair use is permitted.' + '\n'
+copyrightMessage = ('This game is property of Murray Coueslant. Any enquiries can be sent to \
+                     murray.coueslant1@marist.edu. Fair use is permitted.' + '\n')
 mapLocations = ['a', 'b', 'c', 'd', 'e', 'f']
 northCommands = ['n', 'north', 'go north', 'move north', 'travel north']
 eastCommands = ['e', 'east', 'go east', 'move east', 'travel east']
@@ -125,88 +191,17 @@ westCommands = ['w', 'west', 'go west', 'move west', 'travel west']
 helpCommands = ['h', 'help', 'help me', 'get help']
 quitCommands = ['q', 'quit', 'exit', 'end', 'leave']
 
-
-# general game modules
-
-def collisionMessage(player):
-    print('Collision, you cannot move this way. Choose another direction', player.name + '.')
-    return
-
-
-def displayError(messageNo, player):
-    if messageNo == 1:
-        message = ('Incorrect direction command entered, please enter another,', player.name)
-    else:
-        message = 'Unknown error.'
-    print(message)
-
-
-def displayHelp():
-    print('Help:', '\n', 'Enter a command in the prompt, the possible commands are:', '\n', 'north, south, east, west'
-          '\n', 'go, move or travel + a direction', '\n', 'quit, exit, leave, end', '\n'
-          'or this help command, but you figured that one out, go you!')
-
-
-def getCommand(player, command):
-    if command.lower() in northCommands:
-        player.movePlayer('north', gameMap)
-    elif command.lower() in eastCommands:
-        player.movePlayer('east', gameMap)
-    elif command.lower() in southCommands:
-        player.movePlayer('south', gameMap)
-    elif command.lower() in westCommands:
-        player.movePlayer('west', gameMap)
-    elif command.lower() in helpCommands:
-        displayHelp()
-    elif command.lower() in quitCommands:
-        input('Thanks for playing, press enter to end the game.')
-        quit()
-    elif command.lower() == '' or None:
-        print('Unrecognised command, enter another.')
-        getCommand(character, input('Enter new command: '))
-    else:
-        print('Unrecognised command, enter another.')
-        getCommand(character, input('Enter new command: '))
-
-
-def visitLocation(locationRow, locationCol, player):
-    if gameMap.getVisited(locationRow, locationCol) == 'Flag':
-        gameMap.setVisited(locationRow, locationCol)
-    elif gameMap.getVisited(locationRow, locationCol) is False:
-        player.increaseScore()
-        gameMap.setVisited(locationRow, locationCol)
-    elif gameMap.getVisited(locationRow, locationCol) is True:
-        print('You have already discovered this location!')
-
-
-def checkVisited(mapObject):
-    visitedCount = 0
-    map = mapObject.getMap()
-    cols, rows = mapObject.getSizes()
-    for j in range(cols):
-        for i in range(rows):
-            if map[i][j][1] is True:
-                visitedCount += 1
-    return visitedCount
-
-
-def endGame():
-    print('\n' + 'Congratulations, you have explored the whole island!'+'\n')
-    print(copyright)
-    print('I hope you enjoyed playing this game. See you soon!' + '\n')
-    quit()
-
-
+game = game()
 gameMap = map(3, 3, mapLocations)
-character = Player(input('Enter the name of your character: '), 0, 1, 1)
+
+character = Player(input('Enter the name of your character: '), 0, int(gameMap.rowSize/2), int(gameMap.colSize/2))
 character.getLocation(gameMap)
-visitLocation(1, 1, character)
+character.visitLocation()
 character.displayScore()
 while 1:
-    getCommand(character, input('What would you like to do?: '))
+    game.getCommand(character, input('What would you like to do?: '), gameMap)
     character.getLocation(gameMap)
     character.displayScore()
-    count = checkVisited(gameMap)
+    count = gameMap.checkVisited()
     if count == gameMap.rowSize * gameMap.colSize:
-        # character.displayScore()
-        endGame()
+        game.endGame()
