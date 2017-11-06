e44020f9bf989123f94affb075b028cd97dcd07a
Murray Coueslant <murray.coueslant+github@gmail.com>
2017-11-06 11:27:32 -0500

    made it most of the way to printing only the short location until the user types a look command. only problem is that the location is printed twice when the command is used.

--- game.py
+++ game.py
@@ -5,58 +5,6 @@ from pyfiglet import figlet_format
 from termcolor import cprint
 from random import shuffle, randint
 
-# location definitions, these are the places the player can visit
-
-loc1 = ('a sandy beach, the waves lap onto the shore steadily. You look to the horizon and see nothing but the blue '
-        'expanse of the ocean. You contemplate how you got here, and how you are going to get home.')
-
-loc1Short = 'Sandy Beach'
-
-loc2 = ('a dense rainforest. The sound of creatures in the bush is overwhelming, the smell is tropical. '
-        'You are afraid, a predator could appear at any time. You think about what would happen if you did '
-        'not return home.')
-
-loc2Short = 'Dense Rainforest'
-
-loc3 = ('an open clearing. On the ground in front of you there is a pile of strange stones. You are not sure what '
-        'they are for. You wonder if there were once people here, and if so, where are they now?')
-
-loc3Short = 'Open Clearing'
-
-loc4 = ('a roaring waterfall. The white wash rolls from the top of the collosal stones, plunging into a pool of dark '
-        'water. You wonder if there is anything down there, or maybe there is a secret passage behind the falls. '
-        'You attempt to find the passage, but your luck comes up dry.')
-
-loc4Short = 'Roaring Waterfall'
-
-loc5 = ('a strange cave front. There are remnants of exploration here, makeshift torches and tools. You decide that '
-        'it is likely not sensible to go into the cave without proper protection. You guess that people have before '
-        'you; and it has not ended well.')
-
-loc5Short = 'Strange Cave Front'
-
-loc6 = ('a decrepit marine dock. The wood of the jetty is rotting away, there is a rusting hull of a small sailboat '
-        'which is somehow still tied to the jetty. You wonder whether this was once the only connection this '
-        'island had to the outside world.')
-
-loc6Short = 'Decrepit Marine Dock'
-
-loc7 = ('an abandoned hut. The side of the hut has strange images painted on it in red paint, and there are no windows'
-        'on any side. The door sits ajar, you pry it open to reveal some more bizarre paintings on the wall as well as'
-        'hundreds of papers on the floor. You decide not to enter any farther into the structure for fear of crazies.')
-
-loc7Short = 'Abandoned Hut'
-
-loc8 = ('a little creek. The water rushes by and you see some small fish glimmering in the sunlight. You dip your hands'
-        'into the water and take a drink. The water is cold and fresh, you fill your vessel from the creek and carry'
-        'on ahead.')
-
-loc8Short = 'Little Creek'
-
-loc9 = ('a fallen tree. The collosal trunk is hollow after all of the time spent laying on the ground. There are '
-        'clearly animals who call this trunk their home. You decide to move on, you\'d rather not meet any of them.')
-
-loc9Short = 'Fallen Tree'
 
 # variable definitions, these are things which are used often like message strings etc... or things which would make
 # code look ugly if used often in their normal form
@@ -66,6 +14,7 @@ introduction = ('Welcome to a text adventure game. You are a lonely wanderer who
 ending1 = '\nCongratulations, you have explored the whole island!\n'
 ending2 = '\nUnfortunately, you have run out of moves!\n'
 ending3 = 'I hope you enjoyed playing this game. See you soon!\n'
+ending4 = '\nSuccessfully quitting game, thank you for playing.\n'
 copyrightMessage = ('This game is property of Murray Coueslant. Any enquiries can be sent to '
                     'murray.coueslant1@marist.edu. Fair use is permitted.\n')
 helpMessage = ('Help:\nEnter a command below, the possible commands are:\n\tnorth, south, '
@@ -73,8 +22,44 @@ helpMessage = ('Help:\nEnter a command below, the possible commands are:\n\tnort
                'points, score or total\nor this help command, but you figured that one out, go you!')
 
 # location set definition
-mapLocations = [loc1, loc2, loc3, loc4, loc5, loc6, loc7, loc8, loc9]
-shortLocations = [loc1Short, loc2Short, loc3Short, loc4Short, loc5Short, loc6Short, loc7Short, loc8Short, loc9Short]
+mapLocations = [('a sandy beach, the waves lap onto the shore steadily. You look to the horizon and see nothing but '
+                 'the blue expanse of the ocean. You contemplate how you got here, and how you are going to get home.'),
+                ('a dense rain forest. The sound of creatures in the bush is overwhelming, the smell is tropical. '
+                 'You are afraid, a predator could appear at any time. You think about what would happen if you did '
+                 'not return home.'),
+                ('an open clearing. On the ground in front of you there is a pile of strange stones. You are not '
+                 'sure what they are for. You wonder if there were once people here, and if so, where are they now?'),
+                ('a roaring waterfall. The white wash rolls from the top of the collosal stones, plunging into a '
+                 'pool of dark water. You wonder if there is anything down there, or maybe there is a secret passage '
+                 'behind the falls. You attempt to find the passage, but your luck comes up dry.'),
+                ('a strange cave front. There are remnants of exploration here, makeshift torches and tools. '
+                 'You decide that it is likely not sensible to go into the cave without proper protection. '
+                 'You guess that people have before you; and it has not ended well.'),
+                ('a decrepit marine dock. The wood of the jetty is rotting away, there is a rusting hull of a small '
+                 'sailboat which is somehow still tied to the jetty. You wonder whether this was once the only '
+                 'connection this island had to the outside world.'),
+                ('an abandoned hut. The side of the hut has strange images painted on it in red paint, and there are '
+                 'no windows on any side. The door sits ajar, you pry it open to reveal some more bizarre paintings '
+                 'on the wall as well as hundreds of papers on the floor. You decide not to enter any farther into '
+                 'the structure for fear of crazies.'),
+                ('a little creek. The water rushes by and you see some small fish glimmering in the sunlight. '
+                 'You dip your hands into the water and take a drink. The water is cold and fresh, you fill your '
+                 'vessel from the creek and carry on ahead.'),
+                ('a fallen tree. The collosal trunk is hollow after all of the time spent laying on the ground. '
+                 'There are clearly animals who call this trunk their home. You decide to move on, you\'d rather not '
+                 'meet any of them.'),
+                ('a huge totem. There are bizarre symbols carved into the sculpture. Maybe you will return to figure '
+                 'out what they mean.')]
+shortLocations = ['Sandy Beach',
+                  'Dense Rain forest',
+                  'Open Clearing',
+                  'Roaring Waterfall',
+                  'Strange Cave Front',
+                  'Decrepit Marine Dock',
+                  'Abandoned Hut',
+                  'Little Creek',
+                  'Fallen Tree',
+                  'Huge Totem']
 
 # command set definitions
 northCommands = ['n', 'north', 'go north', 'move north', 'travel north']
@@ -87,6 +72,8 @@ scoreCommands = ['score', 'points', 'total']
 yesCommands = ['y', 'yes', 'yep', 'yeah', 'okay', 'please']
 noCommands = ['n', 'no', 'nope', 'nah', 'no thanks']
 quitCommands = ['q', 'quit', 'exit', 'end', 'leave']
+lookCommands = ['look', 'look around', 'view', 'explore']
+searchCommands = ['search', 'search area', 'search location', 'examine']
 
 # player class definition, the player class has a set of methods which apply to the character which the user is
 # controlling
@@ -153,6 +140,12 @@ class Player:
         else:
             print(self.name, 'is currently at', map.getLocation(self))
 
+    def getLongLocation(self, map):
+        if map.getLocation(self) == 'There is nothing here.':
+            print(self.name, 'finds nothing, you should keep exploring.')
+        else:
+            print(self.name, 'is currently at', map.getLongLocation(self))
+
     # this method checks to see if the player has used up all of their available moves for the current game
     def checkMoves(self):
         if self.moves >= self.maxMoves:
@@ -202,7 +195,7 @@ class map:
             randRow, randCol = self.randomRowCol()
             placed = False
 
-            # while loop with a nested if statement which ensures each of the six locations is placed in a distinct
+            # while loop with a nested if statement which ensures each of the ten locations is placed in a distinct
             # location and that two locations are not placed at the same coordinate
             while not placed:
                 if self.map[randRow][randCol] is None:
@@ -300,6 +293,9 @@ class map:
         return self.colSize, self.rowSize
 
     def getLocation(self, player):
+        return self.map[player.rowLocation][player.colLocation][1]
+
+    def getLongLocation(self, player):
         return self.map[player.rowLocation][player.colLocation][0]
 
     def getVisited(self, player):
@@ -367,6 +363,11 @@ class game:
             player.displayScore()
         elif command.lower() in quitCommands:
             self.endGame()
+        elif command.lower() in lookCommands:
+            player.getLongLocation(map)
+            return 'long'
+        elif command.lower() in searchCommands:
+            pass
         elif command.lower() == '' or None:
             self.displayError(3, player)
             self.getCommand(player, input('Enter new command: '), map)
@@ -382,8 +383,11 @@ class game:
         gameMap.visitLocation(player)
         endFlag = False
         while 1:
-            self.getCommand(player, input('\n' + 'What would you like to do?: '), gameMap)
-            player.getLocation(gameMap)
+            locationFlag = self.getCommand(player, input('\n' + 'What would you like to do?: '), gameMap)
+            if locationFlag == 'long':
+                player.getLongLocation(gameMap)
+            else:
+                player.getLocation(gameMap)
             count = gameMap.checkVisited()
             if count == gameMap.rowSize * gameMap.colSize:
                 while endFlag is False:
@@ -407,7 +411,7 @@ class game:
 
 # class instantiations, defines the size of the map and the locations to place in it
 game = game()
-gameMap = map(4, 4, mapLocations, shortLocations)
+gameMap = map(5, 4, mapLocations, shortLocations)
 
 
 # title display routine
